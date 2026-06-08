386915813a3949defc5c8ca53f521a247946a5de
Steve James <federation.design@gmail.com>
2026-06-08 19:16:29 +0100

    Reorder Meet the Pack deck: card44, 11, 14, 21, 8, 28, 36, 10

--- content/cards.ts
+++ content/cards.ts
@@ -3,14 +3,13 @@
 // A card named cardN-<vimeoId>.jpg is a video popout (id parsed from the name).
 export const cards: string[] = [
   "/card.jpg",
-  "/card10-1199436307.jpg",
+  "/card44-1199502279.jpg",
   "/card11-1199268788.jpg",
-  "/card8-1199364230.jpg",
-  "/card28-1199378147.jpg",
   "/card14-1199399283.jpg",
   "/card21-1199411114.jpg",
+  "/card8-1199364230.jpg",
+  "/card28-1199378147.jpg",
   "/card36-1199482204.jpg",
-  "/card44-1199502279.jpg",
+  "/card10-1199436307.jpg",
 ];
-
 export const heroCard = cards[0];
