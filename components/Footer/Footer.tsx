5c942693bc67ef6a76ae13cdb717c52008d71076
Steve James <federation.design@gmail.com>
2026-06-09 14:37:46 +0100

    Centre pitch content on mobile, simplify footer to centred logo, rotate + hide pre-order badge

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,28 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./Footer.module.css";
-
-const menu = [
-  { label: "Homepage", href: "/" },
-  { label: "About", href: "/about" },
-  { label: "Order", href: "/order" },
-  { label: "Contact", href: "/contact" },
-];
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.inner}>
-        <div className={styles.logoCol}>
-          <Image src="/dogbingo.svg" alt="Pedigree Chums" width={340} height={148} />
-        </div>
-        <nav className={styles.menu}>
-          {menu.map((l) => (
-            <Link key={l.href} href={l.href} className={styles.link}>
-              {l.label}
-            </Link>
-          ))}
-        </nav>
+      <div className={styles.logoCol}>
+        <Image src="/dogbingo.svg" alt="Pedigree Chums" width={340} height={148} />
       </div>
 
       <p className={styles.legal}>
