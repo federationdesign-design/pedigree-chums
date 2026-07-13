67d2d2b1299edee49e2888d737beda7a72423317
Steve James <federation.design@gmail.com>
2026-07-13 14:45:00 +0100

    fix: remove extra closing div in sidebar

--- app/smarter-than-the-test/page.tsx
+++ app/smarter-than-the-test/page.tsx
@@ -207,7 +207,6 @@ export default function SmarterThanTheTestPage() {
                 })}
               </div>
             </div>
-            </div>
           </aside>
         </div>
       </main>
